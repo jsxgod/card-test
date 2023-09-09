e2fe029048e8644e5305a27feecb0f9666fd8d93
Kacper Smyczyk <kacpersmyczyk@gmail.com>
2023-09-09 18:03:24 +0200

    feat: load google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "styles/style.globals";
+import Header from "2_Sections/Header/Header";
+import { Kanit } from "@next/font/google";
+
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "600", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${kanit.className}`}>
       <GlobalStyles />
+      <Header />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
